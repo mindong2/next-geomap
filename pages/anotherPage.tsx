60868acb899b4a808c9001a90ed31516f64e1192
mindong2 <qlsnaos12@naver.com>
2023-04-03 23:12:31 +0900

    test

---
+++ pages/anotherPage.tsx
@@ -0,0 +1,7 @@
+export default function Another() {
+  return (
+    <>
+      <h1 style={{ marginTop: "2rem", fontSize: "2rem" }}>another page</h1>
+    </>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,14 @@
+import Another from "./anotherPage";
 export default function Home() {
-  return <>hello World</>;
+  type Remote<T> = (x: T) => T;
+
+  const remoteChange: Remote<string> = (name) => {
+    return `${name}, hello world!`;
+  };
+  return (
+    <>
+      <h1 style={{ fontSize: "3rem" }}>{remoteChange("dongmin")}</h1>
+      <Another />
+    </>
+  );
 }
