4acf2b132d1a6a11bec76174f9312bf02dd0cdb9
Dongmin Kim <qlsnaos12@naver.com>
2023-04-06 18:28:31 +0900

    build and settings

---
+++ pages/theory/getStaticProps.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const getStaticProps = () => {
+    return (
+        <div>
+            <h1>getStaticProps</h1>
+        </div>
+    );
+};
+
+export default getStaticProps;  
